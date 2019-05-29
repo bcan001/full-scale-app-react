14ec0680479818f592f0d4ce42a3533248f9b77c
ben <ben@bens-MBP.localdomain>
2019-05-29 16:04:07 -0500

    added redirect to new page from homepage

--- src/Todo/todoStatus.js
+++ src/Todo/todoStatus.js
@@ -4,11 +4,16 @@ import React from 'react';
 
 const todoStatus = () => {
   return (
-    
     <>
-      <button style={{ flex: 1, padding: 10 }} type="button">All</button>
-      <button style={{ flex: 1, padding: 10 }} type="button">Pending</button>
-      <button style={{ flex: 1, padding: 10 }} type="button">Completed</button>
+      <button style={{ flex: 1, padding: 10 }} type="button">
+        All
+      </button>
+      <button style={{ flex: 1, padding: 10 }} type="button">
+        Pending
+      </button>
+      <button style={{ flex: 1, padding: 10 }} type="button">
+        Completed
+      </button>
     </>
   );
 };
