5015ef6e3be13fe5178b9aa7e309b5644c7cf0a2
ben <ben@bens-mbp.attlocal.net>
2019-05-28 22:47:11 -0500

    finished api calls to backend for main app.

---
+++ src/Screens/Users/index.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+export default class index extends Component {
+  static propTypes = {};
+
+  state = {};
+
+  render() {
+    return (
+      <div>
+        <h1>Users Page</h1>
+      </div>
+    );
+  }
+}
